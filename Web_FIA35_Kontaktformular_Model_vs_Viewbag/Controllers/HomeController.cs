e13193a50306e743f4ea3ac77ec40b7fd980e8fd
Thorsten Diederichs <DiederichsT@bfwob.de>
2021-09-27 14:36:30 +0200

    Projektdateien hinzufügen.

---
+++ Web_FIA35_Kontaktformular_Model_vs_Viewbag/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_FIA35_Kontaktformular_Model_vs_Viewbag.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public IActionResult Index(string inpNameVB, string inpLieblingsfarbeVB, string inpNameModel, string inpLieblingsfarbeModel)
+        {
+            ViewBag.Name = inpNameVB;
+            ViewBag.Lieblingsfarbe = inpLieblingsfarbeVB;
+            Person person = new Person { name = inpNameModel, lieblingsFarbe = inpLieblingsfarbeModel };
+
+            return View(person);
+        }
+    }
+}
---
+++ Web_FIA35_Kontaktformular_Model_vs_Viewbag/Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_FIA35_Kontaktformular_Model_vs_Viewbag
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public string lieblingsFarbe { get; set; }
+    }
+}
